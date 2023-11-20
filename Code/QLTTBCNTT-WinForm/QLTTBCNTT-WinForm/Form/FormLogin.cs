aa4f155eea90a5c60e54f61303480a6c69be9bdb
Your Name <you@example.com>
2023-11-20 20:21:37 +0700

    #12 - feature-login

--- Code/QLTTBCNTT-WinForm/QLTTBCNTT-WinForm/Form/FormLogin.cs
+++ Code/QLTTBCNTT-WinForm/QLTTBCNTT-WinForm/Form/FormLogin.cs
@@ -11,12 +11,58 @@ using System.Windows.Forms;
 
 namespace QLTTBCNTT_WinForm
 {
-    public partial class form : Form
+    public partial class FormLogin : Form
     {
-        public form()
+        private string account = "", pass = "";
+        private QueryTK SQL = new QueryTK();
+        public FormLogin()
         {
             InitializeComponent();
         }
-     
+        #region Event
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+
+            account = txtAcc.Text;
+            pass = txtPass.Text;
+            bool b = false;
+            FormMain fm = new FormMain(account);
+
+            try
+            {
+                b = SQL.isTruePassword(account, pass);
+            }
+            catch
+            {
+                this.Close();
+            }
+               // MessageBox.Show(b.ToString());
+
+            if (b)
+            {
+                txtPass.Text = "●●●●●●";
+                this.Hide();
+                fm.ShowDialog();
+                this.Show();
+            }
+            else
+            {
+                fm.Close();
+                MessageBox.Show("Thông tin đăng nhập không chính xác");
+            }
+
+        }
+
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+                txtPass.UseSystemPasswordChar = !checkBox1.Checked;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        #endregion
     }
 }
